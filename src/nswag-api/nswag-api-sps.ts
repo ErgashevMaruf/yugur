cffe5a3ebf1f2fcd2a07b20815c1c9c060b20660
ErgashevMaruf! <dvoryanin01041998@gmail.com>
2024-12-16 14:31:37 +0000

    Update TypeScript client files from backend

--- src/nswag-api/nswag-api-sps.ts
+++ src/nswag-api/nswag-api-sps.ts
@@ -344,7 +344,7 @@ export interface ISPClient {
     addUpdate(tableName: string | null, model: SPUpdateModel): Observable<FileResponse | null>;
     getSpById(tableName: string | null, id: number): Observable<BaseSPEntity>;
     getSpByIds(tableName: string | null, ids: number[]): Observable<SPSelectModel[]>;
-    getAllRaceDistances(distance: string | null | undefined): Observable<SPSelectModel[]>;
+    getAllRaceDistances(distance: string | null | undefined): Observable<FileResponse | null>;
 }
 
 @Injectable({
@@ -642,7 +642,7 @@ export class SPClient implements ISPClient {
         return _observableOf(null as any);
     }
 
-    getAllRaceDistances(distance: string | null | undefined): Observable<SPSelectModel[]> {
+    getAllRaceDistances(distance: string | null | undefined): Observable<FileResponse | null> {
         let url_ = this.baseUrl + "/api/sps/SP/GetAllRaceDistances?";
         if (distance !== undefined && distance !== null)
             url_ += "distance=" + encodeURIComponent("" + distance) + "&";
@@ -652,7 +652,7 @@ export class SPClient implements ISPClient {
             observe: "response",
             responseType: "blob",
             headers: new HttpHeaders({
-                "Accept": "application/json"
+                "Accept": "application/octet-stream"
             })
         };
 
@@ -663,34 +663,31 @@ export class SPClient implements ISPClient {
                 try {
                     return this.processGetAllRaceDistances(response_ as any);
                 } catch (e) {
-                    return _observableThrow(e) as any as Observable<SPSelectModel[]>;
+                    return _observableThrow(e) as any as Observable<FileResponse | null>;
                 }
             } else
-                return _observableThrow(response_) as any as Observable<SPSelectModel[]>;
+                return _observableThrow(response_) as any as Observable<FileResponse | null>;
         }));
     }
 
-    protected processGetAllRaceDistances(response: HttpResponseBase): Observable<SPSelectModel[]> {
+    protected processGetAllRaceDistances(response: HttpResponseBase): Observable<FileResponse | null> {
         const status = response.status;
         const responseBlob =
             response instanceof HttpResponse ? response.body :
             (response as any).error instanceof Blob ? (response as any).error : undefined;
 
         let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }}
-        if (status === 200) {
-            return blobToText(responseBlob).pipe(_observableMergeMap((_responseText: string) => {
-            let result200: any = null;
-            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
-            if (Array.isArray(resultData200)) {
-                result200 = [] as any;
-                for (let item of resultData200)
-                    result200!.push(SPSelectModel.fromJS(item));
+        if (status === 200 || status === 206) {
+            const contentDisposition = response.headers ? response.headers.get("content-disposition") : undefined;
+            let fileNameMatch = contentDisposition ? /filename\*=(?:(\\?['"])(.*?)\1|(?:[^\s]+'.*?')?([^;\n]*))/g.exec(contentDisposition) : undefined;
+            let fileName = fileNameMatch && fileNameMatch.length > 1 ? fileNameMatch[3] || fileNameMatch[2] : undefined;
+            if (fileName) {
+                fileName = decodeURIComponent(fileName);
+            } else {
+                fileNameMatch = contentDisposition ? /filename="?([^"]*?)"?(;|$)/g.exec(contentDisposition) : undefined;
+                fileName = fileNameMatch && fileNameMatch.length > 1 ? fileNameMatch[1] : undefined;
             }
-            else {
-                result200 = <any>null;
-            }
-            return _observableOf(result200);
-            }));
+            return _observableOf({ fileName: fileName, data: responseBlob as any, status: status, headers: _headers });
         } else if (status !== 200 && status !== 204) {
             return blobToText(responseBlob).pipe(_observableMergeMap((_responseText: string) => {
             return throwException("An unexpected server error occurred.", status, _responseText, _headers);
